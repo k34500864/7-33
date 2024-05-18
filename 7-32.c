f5d9f0d936d2045c9dcaf4a6c4141828d2acb7f1
廷宇 蘇 <k34500864@outlook.com>
2024-05-18 23:35:37 +0800

    加入專案檔案。

---
+++ 7-32.c
@@ -0,0 +1,29 @@
+﻿#include<stdio.h>
+#include<stdbool.h>
+int count(int num);
+int main(void) {
+	int num, sum;
+	scanf("%d", &num);
+	sum = count(num);
+	printf("小於%d的最大值數:%d", num, sum);
+}
+int count(int num) {
+	int i, j;
+	for (i = num-1; i >= 2; i--)
+	{
+		bool logic = true;
+		for (j = 2; j*j <= i; j++)
+		{
+			if (i % j == 0)
+			{
+				logic = false;
+				break;
+			}
+		}
+		if (logic)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
